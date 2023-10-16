fa6111ce49b9d3c56c4cbc919330becad02102b0
Maria <mariafcknt@outlook.com>
2023-10-16 11:06:06 -0300

    Add files via upload

---
+++ script-iac2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Atualizando o servidor e instalando os pacotes necessários..."
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+
